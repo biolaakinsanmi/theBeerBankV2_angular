bfd17c839e53eaaefa20c7fc64ddd3b2e3d8ac6d
biolaakinsanmi <biola.akinsanmi@gmail.com>
2019-02-04 02:14:47 +0100

    fetch and display beers #wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { BeerService } from './_services/beer.service';
+import { Component, OnInit} from '@angular/core';
+import { Subscription } from 'rxjs'
+import { Beer } from './_models/beer-model';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  
+export class AppComponent implements OnInit {
+
+  constructor (
+    private beerService: BeerService
+  ){
+
+  }
+
+  private beers$ : Subscription;
+  public beers: Array<Beer> = [];
+  private page: number = 1;
+  private size: number = 25;
+
+  ngOnInit(){
+    this.beers$ = this.beerService._beers.subscribe(
+      (beersList) => {
+        this.beers = beersList;
+        console.log(beersList);
+      }
+    )
+
+    this.fetchBeersList();
+  }
+
+  fetchBeersList(){
+    this.beerService.fetchBeers()
+  }
+
+
 }
