80f5c97e487e3be96902464225f3d7884bd0063e
biolaakinsanmi <biola.akinsanmi@gmail.com>
2019-02-04 02:14:23 +0100

    Create beer service

--- src/app/_services/beer.service.ts
+++ src/app/_services/beer.service.ts
@@ -1,9 +1,45 @@
+import { Beer } from './../_models/beer-model';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of, BehaviorSubject, observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class BeerService {
 
-  constructor() { }
+  
+  private beers : Array<Beer> = []; 
+  public _beers: BehaviorSubject<Array<Beer>> = new BehaviorSubject([]);
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getBeers(){
+    return this._beers.asObservable();
+  }
+
+  addBeer(newBeer: Beer){
+    this.beers.push(newBeer);
+    return this._beers.next(Object.assign({}, this.beers));
+  }
+
+
+  /*********** HTTP CALLS ******/
+  fetchBeers(page = 1, size = 25){
+    this.http.get(environment.apiUrl + 'beers?page=' + page + '&per_page=' + size).subscribe(
+      (response: any)=>{
+        this.beers = response;
+        this._beers.next(this.beers);
+        return of(true);
+      },
+      (error)=>{
+        return of(false);
+      }
+    )
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
+
 import { AppComponent } from './app.component';
+import { BeerService } from './_services/beer.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
     HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    BeerService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
