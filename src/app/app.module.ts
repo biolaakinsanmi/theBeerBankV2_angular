464978a523363a59c10b5e7241a1cfb386b33e0f
biolaakinsanmi <biola.akinsanmi@gmail.com>
2019-02-04 02:35:05 +0100

    created components for pages home, favourites and pagenotfound

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,17 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { BeerService } from './_services/beer.service';
+import { HomePageComponent } from './home-page/home-page.component';
+import { FavouritePageComponent } from './favourite-page/favourite-page.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent
+    AppComponent,
+    HomePageComponent,
+    FavouritePageComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
